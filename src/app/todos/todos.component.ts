55fb4a224ff62215d2185fdfd319e510eacb8401
zondayang <u226699@gmail.com>
2019-10-23 21:51:45 +0800

    Add new class: Todo

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Todo} from '../todo';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-  todo = "Coding"
+  todo: Todo = {
+    title: "Coding",
+    description: "Write some code.",
+  }
 
   constructor() { }
 
